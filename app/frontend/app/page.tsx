ef737b1039c31f216805e3bbdf6f78ca2523856b
rjzarate <zarate_rj@yahoo.com>
2025-03-26 18:08:17 -0700

    login user test home page

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,10 +1,42 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import Navbar from './components/Navbar';
 
+async function userLoggedIn() {
+  const url = `http://localhost:12094/example_protected_route`;
+  
+  const response = await fetch(url, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+  const json = await response.json();
+  return json;
+}
+
 export default function Intro() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function checkLogin() {
+      const userData = await userLoggedIn();
+      console.log(userData);
+      console.log("test");
+      setUser(userData);
+    }
+
+    checkLogin();
+  }, []);
+
   return (
     <div>
         <Navbar />
-        HIII
+        {user ? <p>Welcome, {user.email}</p> : <p>Not logged in</p>}
     </div>
   );
 }
